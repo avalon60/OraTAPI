85a9b9179f9091f807f8a45c9a7c50d5c004b47c
avalon60 <cbostock20@gmail.com>
2024-12-23 16:59:09 +0000

    Debug installation setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@
 #   Name: setup.sh
 #  Descr: Script to set up the application environment, including creating a
 #         virtual environment, checking/installing pip, installing dependencies,
-#         and configuring scripts.
+#         and configuring scripts. Determines Python interpreter based on the
+#         system.
 #------------------------------------------------------------------------------
 
 realpath() {
@@ -26,6 +27,25 @@ set -e
 
 pushd "${APP_HOME}"
 
+# Determine Python interpreter
+if [ "${OS}" = "Windows_NT" ]
+then
+    PYTHON="python"
+    SOURCE_DIR="Scripts"
+elif command -v python3 >/dev/null 2>&1
+then
+    PYTHON="python3"
+    SOURCE_DIR="bin"
+fi
+
+${PYTHON} --version 2> /dev/null
+if [ $? -ne 0 ]
+then
+    echo "Error: Neither python3 nor python is installed."
+    exit 1
+fi
+echo "Using Python interpreter: $PYTHON"
+
 # Define variables
 VENV_DIR="venv"  # Change this if you want a different name for the virtual env
 BIN_DIR="bin"    # Directory containing shell scripts
@@ -34,11 +54,10 @@ BIN_DIR="bin"    # Directory containing shell scripts
 let step=${step}+1
 step_desc="Check if pip is installed"
 echo "Step ${step}: ${step_desc}..."
-if ! command -v pip >/dev/null 2>&1
-then
+if ! command -v pip >/dev/null 2>&1; then
     echo "pip not found. Installing pip..."
     curl -O https://bootstrap.pypa.io/get-pip.py
-    python3 get-pip.py
+    $PYTHON get-pip.py
     rm get-pip.py
 else
     echo "pip is already installed."
@@ -48,10 +67,9 @@ fi
 let step=${step}+1
 step_desc="Create virtual environment if it doesn't exist"
 echo "Step ${step}: ${step_desc}..."
-if [ ! -d "$VENV_DIR" ]
-then
+if [ ! -d "$VENV_DIR" ]; then
     echo "Creating virtual environment in: $VENV_DIR"
-    python3 -m venv "$VENV_DIR"
+    $PYTHON -m venv "$VENV_DIR"
 else
     echo "Virtual environment already exists in: $VENV_DIR"
 fi
@@ -61,19 +79,20 @@ let step=${step}+1
 step_desc="Activate the virtual environment"
 echo "Step ${step}: ${step_desc}..."
 echo "Activating virtual environment..."
-source "$VENV_DIR/bin/activate"
+source "${VENV_DIR}/${SOURCE_DIR}/activate"
 
 # Step 4: Upgrade pip to ensure you're using the latest version
 echo "Upgrading pip..."
-pip install --upgrade pip
+venv/${SOURCE_DIR}/python -m pip install --upgrade pip
+
 
 # Step 5: Perform the packages install
 step_desc="Perform the packages install"
 let step=${step}+1
 echo "Step ${step}: ${step_desc}..."
-python3 -m pip install .
+$PYTHON -m pip install .
 
-# Step 6: Perform the packages install
+# Step 6: Set executable permissions for shell scripts
 step_desc="Set executable permissions for shell script"
 let step=${step}+1
 echo "Step ${step}: ${step_desc}..."
@@ -84,3 +103,4 @@ chmod +x "$BIN_DIR/ora_tapi.sh"
 echo "Setup completed successfully!"
 
 popd
+
