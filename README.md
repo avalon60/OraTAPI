90033d1e1e1d249a43e35d7fb1422f307a16132a
avalon60 <cbostock20@gmail.com>
2025-01-15 12:33:00 +0000

    Bump version: 1.4.5 → 1.4.6

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # OraTAPI - Oracle Table API Generator 
 
-Version 1.4.5
+Version 1.4.6
 
 - [OraTAPI - Oracle Table API Generator](#oratapi---oracle-table-api-generator)
   - [About OraTAPI](#about-oratapi)
--- src/controller/quick_config.py
+++ src/controller/quick_config.py
@@ -7,7 +7,7 @@ Description: Bootstrap utility to initialise config and template files from reso
 __author__ = "Clive Bostock"
 __date__ = "2024-12-31"
 __description__ = "Configuration bootstrap utility for OraTAPI"
-__version__ = "1.4.5"
+__version__ = "1.4.6"
 
 import argparse
 import shutil
