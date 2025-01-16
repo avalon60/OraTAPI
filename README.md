35d90cf4363948552c014c5446c1819e17d98a18
avalon60 <cbostock20@gmail.com>
2025-01-16 12:27:35 +0000

    Bump version: 1.4.12 → 1.4.13

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # OraTAPI - Oracle Table API Generator 
 
-Version 1.4.12
+Version 1.4.13
 
 - [OraTAPI - Oracle Table API Generator](#oratapi---oracle-table-api-generator)
   - [About OraTAPI](#about-oratapi)
--- src/controller/migrate_config.py
+++ src/controller/migrate_config.py
@@ -7,7 +7,7 @@ Description: Script to migrate, export, or import configuration and template fil
 __author__ = "Clive Bostock"
 __date__ = "2024-12-31"
 __description__ = "Script to migrate, export, or import configuration and template files for OraTAPI."
-__version__ = "1.4.12"
+__version__ = "1.4.13"
 
 import argparse
 import shutil
