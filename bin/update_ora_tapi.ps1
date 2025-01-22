dc0a350a3047ea5937853b56cc1c89107599a0f1
avalon60 <cbostock20@gmail.com>
2025-01-22 11:56:49 +0000

    Add PowerShell script for update_ora_tapi.py

---
+++ bin/update_ora_tapi.ps1
@@ -0,0 +1,59 @@
+<#
+.SYNOPSIS
+Wrapper script for calling OraTAPI/controller/quick_config.py.
+
+.DESCRIPTION
+This script sets up the environment and executes the initialisation routines - a Python program.
+
+This utility copies `.samplei` files from the resources directory to
+target locations, based on the template_category, and specific copying rules.
+We only initialise, if specific files does not exist, we avoid clobbering them.
+
+For help, use:
+    <OraTAPI-Home>\bin\quick_config.ps1 -h
+
+Before the first execution, ensure you set the execute permissions:
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+
+.AUTHOR
+Clive Bostock
+.DATE
+5 August 2024
+#>
+
+# Define the entry point and directories
+$ENTRY_POINT = "quick_config.py"
+$SCRIPT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Path
+$PROJECT_DIR = Split-Path -Parent $SCRIPT_DIR
+$BIN_DIR = Join-Path $PROJECT_DIR "bin"
+$CONTROL_DIR = Join-Path $PROJECT_DIR "src\controller"
+
+# Activate the virtual environment if the script exists
+if (Test-Path "./venv/Scripts/Activate.ps1") {
+    Write-Host "Activating the virtual environment..."
+    . ./venv/Scripts/Activate.ps1
+    Write-Host "Virtual environment activated successfully."
+} else {
+    Write-Warning "Virtual environment activation script not found. Exiting..."
+    exit 1
+}
+
+# Determine the Python interpreter
+$PYTHON_INTERPRETER = ""
+if (Get-Command python -ErrorAction SilentlyContinue) {
+    $PYTHON_INTERPRETER = "python"
+} elseif (Get-Command py -ErrorAction SilentlyContinue) {
+    $PYTHON_INTERPRETER = "py"
+} elseif (Get-Command python3 -ErrorAction SilentlyContinue) {
+    $PYTHON_INTERPRETER = "python3"
+}
+
+# Error handling if no interpreter found
+if (-not $PYTHON_INTERPRETER) {
+    Write-Error "Error: No compatible Python interpreter found (python3, python, or py)!"
+    exit 1
+}
+
+# Execute the Python program
+Write-Host "Executing Python script..."
+& $PYTHON_INTERPRETER (Join-Path $CONTROL_DIR $ENTRY_POINT) @args
